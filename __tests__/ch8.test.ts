2b0309cf96e22d8a7522bb2904280799af97f672
fam <fam1923@gmail.com>
2024-03-24 09:38:46 +0800

    init

---
+++ __tests__/ch8.test.ts
@@ -0,0 +1,13 @@
+import { BookData, BookCollection } from "../src/ch8";
+
+describe("測試sortByPrice - 以輸入域劃分完全組合覆蓋(ACoC)練習", () => {
+  
+});
+
+describe("測試searchByTag - 以資料流圖主路徑覆蓋(PPC)練習", () => {
+ 
+});
+
+describe("測試writeToStorage - 練習測試替身", () => {
+  
+});
